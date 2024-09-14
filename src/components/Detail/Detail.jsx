57809ea1e1c745a22ee5172a7249ae2997bbf8a6
itzjustfitb <fakhrigajar@gmail.com>
2024-09-15 01:03:05 +0400

    2ci commit

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,36 +1,54 @@
-import React from 'react'
-import detailimg from "../../assets/images/detail/detail.png"
+import React from "react";
+import detailimg from "../../assets/images/detail/detail.png";
 import { AiOutlineLike } from "react-icons/ai";
 import { FaRegCommentDots } from "react-icons/fa";
 const Detail = () => {
-    return (
-        <>
-        <div id="detail">
-<div className="detail__container">
-    <div className="detail__top">
-    How to make a Game look more attractive with New VR & AI Technology
-    </div>
-    <div className="detail__img">
-        <img className='detailimgs' src={detailimg} alt="" />
-    </div>
-    <div className="detail__about">
-  <p className='about__top'>Google has been investing in AI for many years and bringing its benefits to individuals, businesses and communities. Whether it’s publishing state-of-the-art research, building helpful products or developing tools and resources that enable others, we’re committed to making AI accessible to everyone.</p>
-<p className='about__bottom'>We’re now at a pivotal moment in our AI journey. Breakthroughs in generative AI are fundamentally changing how people interact with technology — and at Google, we’ve been responsibly developing large language models so we can safely bring them to our products. Today, we’re excited to share our early progress. Developers and businesses can now try new APIs and products that make it easy, safe and scalable to start building with Google’s best AI models through Google Cloud and a new prototyping environment called MakerSuite. And in Google Workspace, we’re introducing new features that help people harness the power of generative AI to create, connect and collaborate.</p>
-    </div>
-    <div className="detail__icons">
-   <div className="detail__Like">
-   <AiOutlineLike />
-<p>Bəyən</p>
-   </div>
-   <div className="detail__coments">
-   <FaRegCommentDots />
-<p>Yorum </p>
-   </div>
-    </div>
-</div>
+  return (
+    <>
+      <div id="detail">
+        <div className="detail__container">
+          <div className="detail__top">
+            How to make a Game look more attractive with New VR & AI Technology
+          </div>
+          <div className="detail__img">
+            <img className="detailimgs" src={detailimg} alt="" />
+          </div>
+          <div className="detail__about">
+            <p className="about__top">
+              Google has been investing in AI for many years and bringing its
+              benefits to individuals, businesses and communities. Whether it’s
+              publishing state-of-the-art research, building helpful products or
+              developing tools and resources that enable others, we’re committed
+              to making AI accessible to everyone.
+            </p>
+            <p className="about__bottom">
+              We’re now at a pivotal moment in our AI journey. Breakthroughs in
+              generative AI are fundamentally changing how people interact with
+              technology — and at Google, we’ve been responsibly developing
+              large language models so we can safely bring them to our products.
+              Today, we’re excited to share our early progress. Developers and
+              businesses can now try new APIs and products that make it easy,
+              safe and scalable to start building with Google’s best AI models
+              through Google Cloud and a new prototyping environment called
+              MakerSuite. And in Google Workspace, we’re introducing new
+              features that help people harness the power of generative AI to
+              create, connect and collaborate.
+            </p>
+          </div>
+          <div className="detail__icons">
+            <div className="detail__Like">
+              <AiOutlineLike />
+              <p>Bəyən</p>
+            </div>
+            <div className="detail__coments">
+              <FaRegCommentDots />
+              <p>Yorum </p>
+            </div>
+          </div>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default Detail
+export default Detail;
--- src/components/Subscription/Subscription.jsx
+++ src/components/Subscription/Subscription.jsx
@@ -1,27 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Subscription = () => {
   return (
-    <div id='subscription'>
-        <div className="subscription__box">
-            <div className="subscription__container">
-
-<div className="subscription__top">
-Get our stories delivered From us to your inbox weekly.
-</div>
-<div className="subscription__mail">
-<input className='subscription__input' placeholder='Your Email' type="text" name="" id="" />
-<button className='subscription__btn'>
-Get started
-</button>
-</div>
-<div className="subscription__bottom">
-Get a response tomorrow if you submit by 9pm today. If we received after 9pm will get a reponse the following day.
-</div>
-            </div>
+    <div id="subscription">
+      <div className="subscription__box">
+        <div className="subscription__container">
+          <div className="subscription__top">
+            Get our stories delivered From us to your inbox weekly.
+          </div>
+          <div className="subscription__mail">
+            <input
+              className="subscription__input"
+              placeholder="Your Email"
+              type="text"
+              name=""
+              id=""
+            />
+            <button className="subscription__btn">Get started</button>
+          </div>
+          <div className="subscription__bottom">
+            Get a response tomorrow if you submit by 9pm today. If we received
+            after 9pm will get a reponse the following day.
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Subscription
+export default Subscription;
